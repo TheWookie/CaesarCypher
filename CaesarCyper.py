219252b5f8bf8882328e10a52a5d4930392ebd3a
Paul Baker <son.of.lysander@gmail.com>
2013-10-05 19:16:10 -0600

    Sexy progress, Sexier grouping

--- CaesarCyper.py
+++ CaesarCyper.py
@@ -5,7 +5,9 @@ Created on Oct 5, 2013
 '''
 import re
 
-def infixToPostfix(EquationString):
+def InfixToPostfix(EquationString):
+    EquationString = EquationString.upper()
+    #EquationString = re.sub("N", NValue, EquationString) #replace the variable with the actual N value.
     EquationString = re.sub("(\s+)", "", EquationString) #removing unnecessary spaces.
     
     Q = []
@@ -14,19 +16,29 @@ def infixToPostfix(EquationString):
     # ^(\d+) #Number at begging of string regex
     # ^([\+\*-\/\^%]) #Opperator at beggining of string regex
     # ^([\(\)]) #matchs oppening and closing paren.
-    while (len(EquationString) > 0):
-        DigitSearch = re.search(r"^(\d+)", EquationString)
-        OperatorSearch = re.search(r"^([\+\*-\/\^])", EquationString)
+    while len(EquationString) > 0:
+        DigitSearch = re.search(r"^(\d+|N)", EquationString) #tests for digit, OR variable N
         if (DigitSearch):
-            Q.append(int(DigitSearch.group(1)))
+            if DigitSearch.group(1) == "N":
+                Q.append(DigitSearch.group(1))
+            else:
+                Q.append(int(DigitSearch.group(1)))
             EquationString = re.sub(r"^(\d+)", "", EquationString)
             continue
-        elif (OperatorSearch):
+        OperatorSearch = re.search(r"^([\+\*-\/\^])", EquationString)
+        if (OperatorSearch):
             Stk.append(OperatorSearch.group(1))
             EquationString = re.sub(r"^([\+\*-\/\^])", "", EquationString)
             continue
+        #If we reach this line, something has probably gone wrong.
         pass
     return [Q, Stk]
+
+def ParseRPN(RPN, NValue):
+    ParseStk = []
+    for i in range(0, len(RPN)):
+        
+    return
 
 print(infixToPostfix("5 + 4"))
 print(infixToPostfix("5 + 4 - 3"))
