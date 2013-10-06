37e3860f6f8cdf2cc109fb4b05fbbdbbd2d88b75
Paul Baker <son.of.lysander@gmail.com>
2013-10-05 20:41:04 -0600

    Sexy Progress.

--- CaesarCyper.py
+++ CaesarCyper.py
@@ -6,7 +6,16 @@ Created on Oct 5, 2013
 import re
 
 Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-Operators = "^*/+-"
+Operators = {  # Setup Opperators and their priorities. P-lease E-xcuse M-y D-ear A-unt S-ally
+             "+" : 0,
+             "-" : 0,
+             "*" : 1,
+             "/" : 1,
+             "%" : 1,
+             "^" : 2,
+             "(" : 3,
+             ")" : 3 #Both paren same priority? Is this right? We'll see...
+}
 
 def InfixToPostfix(EquationString):
     EquationString = EquationString.upper()
@@ -22,19 +31,24 @@ def InfixToPostfix(EquationString):
     while len(EquationString) > 0:
         DigitSearch = re.search(r"^(\d+|N)", EquationString)  # tests for digit, OR variable N
         if (DigitSearch):
+            EquationString = re.sub(r"^(\d+)", "", EquationString)
             if DigitSearch.group(1) == "N":
                 Q.append(DigitSearch.group(1))
             else:
                 Q.append(int(DigitSearch.group(1)))
-            EquationString = re.sub(r"^(\d+)", "", EquationString)
             continue
         OperatorSearch = re.search(r"^([\+\*-\/\^])", EquationString)
         if (OperatorSearch):
-            Stk.append(OperatorSearch.group(1))
             EquationString = re.sub(r"^([\+\*-\/\^])", "", EquationString)
+            op = OperatorSearch.group(1)
+            while (not len(Stk) == 0 and Operators[Stk[len(Stk) - 1]] >= Operators[op]):
+                Q.append(Stk.pop())
+            Stk.append()
             continue
+        ParenSearch = re.search(r"^([\(\)])")
+        if (ParenSearch):
+            pass
         # If we reach this line, something has probably gone wrong.
-        pass
     return [Q, Stk]
 
 def ParseRPN(RpnArr, NValue):
@@ -45,20 +59,23 @@ def ParseRPN(RpnArr, NValue):
         if (isinstance(RpnArr[i], int)):
             ParseStk.append(RpnArr[i])
         else:
-            if Operators.index(RpnArr[i]):
-                A = ParseStk.pop()
-                B = ParseStk.pop()
-                # I have recently researched a more Pythonic way of doing this,
-                # however to implement it would be plagarism. (This is a homework assignment)
-                # After I turn it in, I'll make this better. (http://stackoverflow.com/questions/1740726/python-turn-string-into-operator)
-                if (RpnArr[i] == "+"):
-                    ParseStk.append(A + B)
-                elif (RpnArr[i] == "-"):
-                    ParseStk.append(B - A)
-                elif (RpnArr[i] == "*"):
-                    ParseStk.append(A * B)
-                elif (RpnArr[i] == "/"):
-                    ParseStk.append(B / A)
+            A = ParseStk.pop()
+            B = ParseStk.pop()
+            # I have recently researched a more Pythonic way of doing this,
+            # however to implement it would be plagarism. (Which sucks because this is a homework assignment)
+            # After I turn it in, I'll make this better. (http://stackoverflow.com/questions/1740726/python-turn-string-into-operator)
+            if (RpnArr[i] == "+"):
+                ParseStk.append(B + A)  # Order doesn't matter here, but putting B ahead of A is consitent with the other conditions.
+            elif (RpnArr[i] == "-"):
+                ParseStk.append(B - A)
+            elif (RpnArr[i] == "*"):  # See immediately previous note.
+                ParseStk.append(B * A)
+            elif (RpnArr[i] == "/"):
+                ParseStk.append(B / A)
+            elif (RpnArr[i] == "%"):
+                ParseStk.append(B % A)
+            elif (RpnArr[i] == "^"):
+                ParseStk.append(A ** B)  # This MUST be in reverse order to perform correctly
     return ParseStk[0]
 
 # eqn = "5 + 4 - 3"
@@ -68,4 +85,4 @@ def ParseRPN(RpnArr, NValue):
 # print(rpn)
 rpn = [9, "N", 7, 3, "-", "/", "+"]
 rslt = ParseRPN(rpn, 24)
-print(rslt) 
+print(rslt)  # the answer should be 15.
