2a029e447ba82fc4e45ca045961dcfb8ac88f6d0
Paul Baker <son.of.lysander@gmail.com>
2013-10-06 01:24:45 -0600

    Sexiest code yet. Works perfectly. InfixToPostfix converter is sexy. The utilization of caesar-cypher is sexy. Now all that's left is to remove the sexy print outs for sexy debugging

--- CaesarCyper.py
+++ CaesarCyper.py
@@ -4,6 +4,11 @@ Created on Oct 5, 2013
 @author: Paul
 '''
 import re
+import sys
+
+DEBUGMODEON = False
+if (sys.gettrace()):  # this little debug diddy comes from here: http://stackoverflow.com/questions/333995/how-to-detect-that-python-code-is-being-executed-through-the-debugger
+    DEBUGMODEON = True
 
 Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 Operators = {  # Setup Opperators and their priorities. P-lease E-xcuse M-y D-ear A-unt S-ally
@@ -14,11 +19,14 @@ Operators = {  # Setup Opperators and their priorities. P-lease E-xcuse M-y D-ea
              "%" : 1,
              "^" : 2,
              "(" :-1,  # Turns out we definitely need paren here for comparisons against operators. HOWEVER they must have a lower priority 
-             ")" :-1  # to allow for checking the stack order correctly. This is okay because paren are handled in a differently anyway.
+             ")" :-1  # to allow for checking the stack order correctly. This is because Paren are not actually operators BUT we have to check against them in our stack. This does not cause any problems because we already have a separate case for handling paren, even though they both use the same stack.
 }
+
+DEBUGPOSTFIX = False
 
 def InfixToPostfix(EquationString):
     EquationString = EquationString.upper()
+    EquationString = re.sub("(MOD)", "%", EquationString)
     EquationString = re.sub("(\s+)", "", EquationString)  # removing unnecessary spaces.
     Q = []
     Stk = []
@@ -26,25 +34,25 @@ def InfixToPostfix(EquationString):
     # ^([\+\*-\/\^%]) #Opperator at beggining of string regex
     # ^([\(\)]) #matchs oppening and closing paren.
     while len(EquationString) > 0:
-        print("New iteration. Sanity check-\t\tQueue: {}, Stack: {}".format(Q, Stk))
-        DigitSearch = re.search(r"^(\d+|N)", EquationString)  # tests for digit, OR variable N
+        if (DEBUGMODEON and DEBUGPOSTFIX):
+            print("\"{0}\"".format(EquationString))
+            print("New iteration. Sanity check-\t\tQueue: {}, Stack: {}".format(Q, Stk))
+        DigitSearch = re.search(r"^(\d+|P)", EquationString)  # tests for digit, OR variable N
         if (DigitSearch):
-            EquationString = re.sub(r"^(\d+|N)", "", EquationString)
-            print("Appending {} to Queue.".format(DigitSearch.group(1)))
-            if DigitSearch.group(1) == "N":
+            EquationString = re.sub(r"^(\d+|P)", "", EquationString)
+            if DigitSearch.group(1) == "P":
                 Q.append(DigitSearch.group(1))
             else:
                 Q.append(int(DigitSearch.group(1)))
+            if (DEBUGMODEON and DEBUGPOSTFIX):
+                print("Enqueing {}".format(DigitSearch.group(1)))
             continue
-        OperatorSearch = re.search(r"^([\+\*-\/\^])", EquationString)
+        OperatorSearch = re.search(r"^([\+\*-\/\^%])", EquationString)
         if (OperatorSearch):
-            EquationString = re.sub(r"^([\+\*-\/\^])", "", EquationString)
+            EquationString = re.sub(r"^([\+\*-\/\^%])", "", EquationString)
             op = OperatorSearch.group(1)
             while (len(Stk) > 0 and Operators[Stk[len(Stk) - 1]] >= Operators[op]):
-                print("Comparing operators {0} and {1}".format(Stk[len(Stk) - 1], op))
-                print("Poping {0} from Stack and appending to Queue.".format(Stk[len(Stk) - 1]))
                 Q.append(Stk.pop())
-            print("Pushing operator {0} to Stack.".format(op))
             Stk.append(op)
             continue
         ParenSearch = re.search(r"^([\(\)])", EquationString)
@@ -52,32 +60,33 @@ def InfixToPostfix(EquationString):
             EquationString = re.sub(r"^([\(\)])", "", EquationString)
             if (ParenSearch.group(1) == "("):
                 Stk.append(ParenSearch.group(1))
-                print("Pushing ( to stack")
             else:  # right paren
-                print("We have a )")
                 while not Stk[len(Stk) - 1] == "(":
-                    print("Popping {0} from stack and appending to q.".format(Stk[len(Stk) - 1]))
                     Q.append(Stk.pop())
-                print("Popping and ditching {0}, because we don't need it anymore.".format(Stk[len(Stk) - 1]))
                 Stk.pop()  # ditch the left paren
             continue
-        print("Something has gone terribly wrong if this has been printed.")  # If we reach this line, something has probably gone wrong. http://www.youtube.com/watch?v=Q5cEAhjcv54
+        raise Exception("Not a valid infix equation. {0}".format(EquationString))  # If we reach this line, something has probably gone wrong. http://www.youtube.com/watch?v=Q5cEAhjcv54
     while len(Stk) > 0:
         Q.append(Stk.pop())
+    if (DEBUGMODEON and DEBUGPOSTFIX):
+        print(Q)
     return Q
 
-def ParseRPN(RpnArr, NValue):
+DEBUGRPN = False
+
+def ParseRPN(RpnArr, PValue):
+    RpnArr = list(RpnArr)  # we need a deep copy. If we don't we'll override P and it'll stay that way through the duration of our encryption.
     ParseStk = []
     for i in range(0, len(RpnArr)):
-        if (RpnArr[i] == "N"):
-            RpnArr[i] = NValue  # make the mathmatical subsititution here so we can correctly evaluate this equation.
+        if (RpnArr[i] == "P"):
+            RpnArr[i] = PValue  # make the mathmatical subsititution here so we can correctly evaluate this equation.
         if (isinstance(RpnArr[i], int)):
             ParseStk.append(RpnArr[i])
         else:
             A = ParseStk.pop()
             B = ParseStk.pop()
             # I have recently researched a more Pythonic way of doing this,
-            # however to implement it would be plagarism. (Which sucks because this is a homework assignment)
+            # however to implement it would be plagarism (because this is a homework assignment.)
             # After I turn it in, I'll make this better. (http://stackoverflow.com/questions/1740726/python-turn-string-into-operator)
             if (RpnArr[i] == "+"):
                 ParseStk.append(B + A)  # Order doesn't matter here, but putting B ahead of A is consitent with the other conditions.
@@ -86,15 +95,44 @@ def ParseRPN(RpnArr, NValue):
             elif (RpnArr[i] == "*"):  # See immediately previous note.
                 ParseStk.append(B * A)
             elif (RpnArr[i] == "/"):
-                ParseStk.append(B / A)  # not forcing integer division for now.
+                ParseStk.append(B // A)  # Forcing integer division. Why? Because for this project (Caesar Cypher) we will need integers so we can index the alphabet.
             elif (RpnArr[i] == "%"):
                 ParseStk.append(B % A)
             elif (RpnArr[i] == "^"):
                 ParseStk.append(A ** B)  # This MUST be in reverse order to perform correctly
     return ParseStk[0]
 
-eqn = "9 + N / ( 7 - 3 )"
-rpn = InfixToPostfix(eqn)  # should be [9, "N", 7, 3, "-", "/", "+"]
-print (rpn)
-rslt = ParseRPN(rpn, 24)
-print(rslt)  # the answer should be 15.
+DEBUGCAESAR = False
+
+def CaesarCypher(message, infix):
+    message = message.upper()
+    CypherText = ""
+    PostFix = InfixToPostfix(infix)
+    if (DEBUGMODEON and DEBUGCAESAR):
+        print(PostFix)
+    for i in range(0, len(message)):
+        if (message[i] in Alphabet):
+            index = Alphabet.index(message[i])
+            if (DEBUGMODEON and DEBUGCAESAR):
+                print("message[{0}] = {1}".format(i, message[i]))
+            newIndex = ParseRPN(PostFix, index)
+            if (DEBUGMODEON and DEBUGCAESAR):
+                print(newIndex)
+            CypherText += Alphabet[newIndex]
+            if (DEBUGMODEON and DEBUGCAESAR):
+                print("Alphabet[{0}] = {1}".format(newIndex, Alphabet[newIndex]))
+        else:
+            CypherText += message[i]  # If the character isn't part of our cypher, like a punctuation mark or a space, we'll just ignore it.
+    return CypherText
+
+def DemoCC(message, key, oppKey):
+    message = CaesarCypher(message, key)
+    print("{} -- {}".format(message, key))
+    message = CaesarCypher(message, oppKey)
+    print("{} -- {}".format(message, oppKey))
+    print()
+    return
+
+DemoCC("The quick brown fox jumps over the lazy dog.", "(P+3) mod 26", "(P-3) mod 26")
+DemoCC("The rain in spain stays mainly on the plane.", "(P+8) mod 26", "(P-8) mod 26")
+DemoCC("Never gunna give you up, never going to let you down.", "(P+(256*12^3004/4+1)) mod 26", "(P-(256*12^3004/4+1)) mod 26")
